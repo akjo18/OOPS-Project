a1e26e2621a179c4fd0c68e98c28bd1a06e2029e
AKSHAT JOHAR <akjo18102002@gmail.com>
2022-12-07 22:19:50 +0530

    get product for manager home page

--- src/pages/ManagerHomePage.js
+++ src/pages/ManagerHomePage.js
@@ -1,41 +1,54 @@
 import { Box, Grid } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ManagerProductCard from "../components/ManagerProductCard";
 import ManagerBar from "../components/ManagerBar";
-
+import axios from "axios";
 function ManagerHomePage() {
   // eslint-disable-next-line no-sparse-arrays
-  const items = [
-    {
-      name: "iPhone 14",
-      price: 80000,
-      qty: 5,
-      id: 1,
-      imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
-    },
-    {
-      name: "Dell Inspiron 14",
-      price: 70000,
-      qty: 4,
-      id: 2,
-      imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
-    },
-    ,
-    {
-      name: "Samsung Galaxy 10",
-      price: 60000,
-      qty: 6,
-      id: 3,
-      imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
-    },
-    {
-      name: "Apple Smart Watch 6",
-      price: 50000,
-      qty: 10,
-      id: 4,
-      imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
-    },
-  ];
+  // const items = [
+  //   {
+  //     name: "iPhone 14",
+  //     price: 80000,
+  //     qty: 5,
+  //     id: 1,
+  //     imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
+  //   },
+  //   {
+  //     name: "Dell Inspiron 14",
+  //     price: 70000,
+  //     qty: 4,
+  //     id: 2,
+  //     imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
+  //   },
+  //   ,
+  //   {
+  //     name: "Samsung Galaxy 10",
+  //     price: 60000,
+  //     qty: 6,
+  //     id: 3,
+  //     imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
+  //   },
+  //   {
+  //     name: "Apple Smart Watch 6",
+  //     price: 50000,
+  //     qty: 10,
+  //     id: 4,
+  //     imgUrl: "https://i.postimg.cc/LszsMdsT/laptop.webp",
+  //   },
+  // ];
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(
+      "http://localhost:8080/viewAllProductsByManager"
+    );
+    setItems(result.data);
+  };
 
   return (
     <div>
